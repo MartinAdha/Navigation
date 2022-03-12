6cb62780310b8bbf26fb526c6c4c89a0cab20c39
Martin Adha <76505063+MartinAdha@users.noreply.github.com>
2022-03-13 02:33:39 +0700

    Initial commit

---
+++ app/src/main/java/com/mar/soloapp/ui/developer/DeveloperFragment.java
@@ -0,0 +1,38 @@
+package com.mar.soloapp.ui.developer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mar.soloapp.databinding.FragmentDeveloperBinding;
+import com.mar.soloapp.databinding.FragmentGalleryBinding;
+
+public class DeveloperFragment extends Fragment {
+
+    private FragmentDeveloperBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DeveloperViewModel galleryViewModel =
+                new ViewModelProvider(this).get(DeveloperViewModel.class);
+
+        binding = FragmentDeveloperBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDeveloper;
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
